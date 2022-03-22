bab585f618391884b67b7f4061429f300d340db9
Polo M2B <polo2bodi@gmail.com>
2022-03-22 09:51:13 +0100

    fake webhook - for bump usage
    Beside following our petrol consumption, I'm also working at bump.sh,;
    where I'm currently implementing webhook support.
    
    This commit add a fake route POST /fake_webhook, to be called from Bump app.

---
+++ app/controllers/bump_controller.rb
@@ -0,0 +1,14 @@
+class BumpController < ApplicationController
+  def fake_webhook
+    # POST
+    # this is a webhook, to be called implemented in bump.sh
+    5.times { puts 'X' * 10 + __method__.to_s + 'X' * 10 }
+    p params
+
+    redirect_to ping_webhook_path, notice: 'POST request done on /fake_webhook'
+  end
+
+  def ping
+    # GET
+  end
+end
